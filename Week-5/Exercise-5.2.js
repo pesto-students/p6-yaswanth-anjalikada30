d5a80144e9211d1314b5fa42d2679b686b58b832
anjalikada30 <anjalikada26@com>
2022-11-06 16:50:24 +0530

    changes for adding matcher to check vowel

--- Week-5/Exercise-5.2.js
+++ Week-5/Exercise-5.2.js
@@ -1,14 +1,16 @@
-function isVowel(char){
-    return 'aeiou'.includes(char);
-}
 function vowelCount(string){
-    const result = new Map()
+    const vowelMatcher = /[aeiou]/gi ;
+    const result = new Map([
+        ['a',0],
+        ['e',0],
+        ['i',0],
+        ['o',0],
+        ['u',0]
+    ])
     for(let char of string){
         char = char.toLowerCase();
-        if(isVowel(char)){
-            if(result.has(char)) 
-                result.set(char, result.get(char)+1)
-            else result.set(char, 1)
+        if(char.match(vowelMatcher)){
+            result.set(char, result.get(char)+1)
         }
     }
     return result;
