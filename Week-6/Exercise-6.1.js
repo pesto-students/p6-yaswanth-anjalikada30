b6866373047b9ad2994bc47300b2b884e83fa224
anjalikada30 <anjalikada26@com>
2022-11-12 19:16:41 +0530

    added TC and SC

--- Week-6/Exercise-6.1.js
+++ Week-6/Exercise-6.1.js
@@ -1,4 +1,10 @@
 const maxSubArray = function(nums) {
+    /*
+        used Kadane's algorithm
+        1) add each value to sum while iterating
+        2) if sum is greater than max replace max with sum
+        3) if sum is less than zero, no need to carry. make it zero
+     */
     let sum = 0, max = nums[0];
     nums.forEach(num=>{
         sum += num;
@@ -11,3 +17,7 @@ const maxSubArray = function(nums) {
 };
 const arr = [1, 2, 3, 4, -10];
 console.log("Max sum contiguous subarray - ", maxSubArray(arr))
+/*
+    Time complexity = O(n)
+    Space complexity = O(1)
+ */
