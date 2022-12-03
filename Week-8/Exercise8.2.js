7d037747c7aa761e1ec65ac3105ea9fd78516805
anjalikada30 <anjalikada26@com>
2022-12-04 00:10:00 +0530

    changes for exercise 2

---
+++ Week-8/Exercise8.2.js
@@ -0,0 +1,18 @@
+const getInorder = (root, inorder) => {
+    if(!root)
+        return;
+    getInorder(root.left, inorder)
+    inorder.push(root.val)
+    getInorder(root.right, inorder)
+}
+const isValidBST = (root) => {
+    const inorder = [];
+    getInorder(root, inorder); 
+    let temp = inorder[0];
+    for(let i = 1; i < inorder.length; i++){
+        if(temp >= inorder[i])
+            return false;
+        else temp = inorder[i]
+    }
+    return true;
+};
